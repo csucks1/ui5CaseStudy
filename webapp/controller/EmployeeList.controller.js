fc3fb64da39ea6ab2734a5d452d49cfc448a84dc
Jolinna Mae Tibulan <jolinna.mae.tibulan@accenture.com>
2025-06-06 06:53:15 +0000

    Feat: Internationalize views and improve layouts

--- webapp/controller/EmployeeList.controller.js
+++ webapp/controller/EmployeeList.controller.js
@@ -8,6 +8,25 @@ sap.ui.define([
         onInit() {
         },
 
+        /**
+         * This function is called every time the table's items are updated.
+         * It updates the title of the table with the number of employees.
+         * @param {sap.ui.base.Event} oEvent The update finished event
+         */
+        onUpdateFinished: function (oEvent) {
+            // Get the total number of items in the table's binding
+            var iTotalItems = oEvent.getParameter("total");
+            
+            // Get the i18n model's resource bundle
+            var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+            
+            // Get the text from the i18n properties file, replacing the placeholder {0} with the total item count
+            var sText = oResourceBundle.getText("employeeCountText", [iTotalItems]);
+            
+            // Find the Text control by its ID and set the updated text
+            this.byId("employeeCountText").setText(sText);
+        },
+
         onAddEmployee: function (){
             var oRouter = this.getOwnerComponent().getRouter();        
             oRouter.navTo("RouteAddEmployee");
